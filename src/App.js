53843d966926747767503ddc5dff24abc55b5f68
Cindewi <112582932+dewismecindy@users.noreply.github.com>
2023-01-10 23:21:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,77 +1,39 @@
 import "./App.css";
-import Header from "./components/Header";
 import { useState } from "react";
 import Footer from "./components/Footer";
 import StepTwo from "./components/StepTwo";
+import Form from "./components/Form";
 
 function App() {
+  const [isValid, setIsValid] = useState(false);
   const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmpassword, setConfirmPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (name && password && email)
-      if (password === confirmpassword) {
-        alert("Votre formulaire a bien été envoyé");
-      } else {
-        alert("Vos deux mots de passe ne sont pas identiques");
-      }
-  };
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   return (
     <div className="App">
-      <Header />
-      <form onSubmit={handleSubmit}>
-        {/* // htmlFor du label doit correspondre à l'id de l'input associé */}
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          placeholder="Jean Dupont"
-          onChange={(event) => {
-            setName(event.target.value);
-          }}
-          id="name"
-          value={name}
+      {isValid === false ? (
+        <Form
+          setState={setIsValid}
+          name={name}
+          setName={setName}
+          email={email}
+          setEmail={setEmail}
+          password={password}
+          setPassword={setPassword}
+          confirmPassword={confirmPassword}
+          setConfirmPassword={setConfirmPassword}
         />
-        <label htmlFor="Email">Email</label>
-        <input
-          type="email"
-          value={email}
-          placeholder="jeandupont@lereacteur.io"
-          id="mail"
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
+      ) : (
+        <StepTwo
+          setState={setIsValid}
+          name={name}
+          email={email}
+          password={password}
         />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          value={password}
-          placeholder="lErEaCtEuR2020"
-          id="password"
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
-        />
-        <label htmlFor="Confirm your password">Confirm your password</label>
-        <input
-          type="password"
-          value={confirmpassword}
-          placeholder="lErEaCtEuR2020"
-          id="confirmpassword"
-          onChange={(event) => {
-            setConfirmPassword(event.target.value);
-          }}
-        />
-        <button type="submit">Register</button>
-      </form>
+      )}
 
-      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-      <footer>
-        <Footer />
-      </footer>
+      <Footer />
     </div>
   );
 }
